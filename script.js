da912c91a885cbcd3d72f9d756ba01fee5d7a8e3
Manohar Kumar <manohar.csengineer@gmail.com>
2023-09-17 23:51:10 +0530

    Completed

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ function typeWriter() {
   }
 }
 setInterval(typeWriter, 100);
+
+// for toggle effect
+var x = document.getElementById("log");
+var y = document.getElementById("reg");
+// take two type of form
+var z = document.getElementById("btn"); // moving button
+
+function register() {
+  x.style.left = "-450px";
+  y.style.left = "0px";
+  z.style.left = "110px";
+}
+function login() {
+  x.style.left = "0px";
+  y.style.left = "450px";
+  z.style.left = "0px";
+}
+
+// for popups
+let pop = document.getElementById("popup");
+
+function openlog() {
+  pop.classList.add("open-popup");
+}
+function closelog() {
+  pop.classList.remove("open-popup");
+}
+
+// for welcome
+let we = document.querySelector(".welcome");
+function welcome() {
+  closelog();
+  we.classList.add("welpop");
+  document.getElementById("log").reset();
+  document.getElementById("reg").reset();
+}
+function closewel() {
+  we.classList.remove("welpop");
+}
